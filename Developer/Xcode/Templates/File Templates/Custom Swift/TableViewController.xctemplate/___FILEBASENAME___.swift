c9fb5bdbcbccbce991a26664e5463b2cb8d74e25
Will Townsend <will@townsend.io>
2016-09-25 13:07:19 +1300

    Fix colon spacing on view controller

--- Developer/Xcode/Templates/File Templates/Custom Swift/TableViewController.xctemplate/___FILEBASENAME___.swift
+++ Developer/Xcode/Templates/File Templates/Custom Swift/TableViewController.xctemplate/___FILEBASENAME___.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class ___FILEBASENAMEASIDENTIFIER___ : UITableViewController {
+class ___FILEBASENAMEASIDENTIFIER___: UITableViewController {
 
     // MARK: - Properties
 
